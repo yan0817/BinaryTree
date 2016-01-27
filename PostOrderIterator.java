1a973076ae10909613aa13b19fa02c02a466dba5
yan0817 <yangelides2016@pingry.org>
2016-01-27 11:26:00 -0500

    Create PostOrderIterator.java

---
+++ PostOrderIterator.java
@@ -0,0 +1,70 @@
+import java.util.NoSuchElementException;
+/**
+@author Yanni Angelides
+@version 10/30/15 
+*/
+
+import java.util.Iterator;
+import java.lang.Iterable;
+
+public class PostOrderIterator<E> implements Iterator<E>
+{
+		private Vector<E> vector = new Vector<E>();
+		private int curr;
+		
+		public PostOrderIterator(BinaryTree<E> tree)
+		{	
+			curr = 0;
+			makeVector(tree);
+		}
+		
+		/**
+		This is a helper method that turns the tree into a Vector. The Vector contains the values of the different trees in the order of the specified iterator.
+		*/
+		public void makeVector(BinaryTree<E> t) 
+		{
+			if(t.isLeaf())
+			{
+				vector.add(t.value()); //want to add the value of the tree to the vector because you have reached the bottom of a tree
+				return;
+			}
+			else
+			{
+				if(t.left() != null)
+				{
+					makeVector(t.left()); //call your self iteratively to go down the left side
+				}
+				if(t.right() != null)
+				{
+					makeVector(t.right());
+				}
+				vector.add(t.value());
+			}
+		}
+		/**
+		Once the BinaryTree is turned into a Vector you can treat this as a Vector iterator. This is a method from the Iterator Interface that returns the next object in the Vector	
+		@return E the next object of type E in the Vector
+		*/
+		public E next()
+		{
+			if (hasNext() == false)
+			{
+				System.out.println("There is no next element in the tree");
+				throw new NoSuchElementException();
+			}
+			else
+			{
+				curr++;
+				return (vector.get(curr-1));
+			}
+		}
+		
+		/**
+		@return boolean determining whether or not there is another object in the Vector
+		*/
+		public boolean hasNext()
+		{
+			return (curr < vector.size());
+		}
+		
+}
